0855dcfe7285fd021f7969c37c07e45e6359d102
lycheng <lycheng997@gmail.com>
2013-09-10 23:07:23 +0800

    p11

---
+++ p11_p12.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+__author__ = "lycheng"
+__email__ = "lycheng997@gmail.com"
+
+from utils import timeit
+
+@timeit(times=1)
+def p11():
+
+    def cal_sub_max_product(m, i, j, x_max, y_max):
+        """
+        calculate sub matrix product
+        """
+        result = 0
+        if x_max - i >= 4:
+            result = max(m[i][j] * m[i+1][j] * m[i+2][j] * m[i+3][j], result)
+
+        if y_max - j >= 4:
+            result = max(m[i][j] * m[i][j+1] * m[i][j+2] * m[i][j+3], result)
+
+        if x_max - i >= 4 and y_max - j >= 4:
+            result = max(m[i][j] * m[i+1][j+1] * m[i+2][j+2] * m[i+3][j+3], result)
+
+        if i >= 3 and y_max - j >= 4:
+            result = max(m[i][j] * m[i-1][j+1] * m[i-2][j+2] * m[i-3][j+3], result)
+
+        return result
+
+    matrix = []
+
+    with open("/home/lycheng/p11") as f:
+        for line in f:
+            nums = map(int, line.split(' '))
+            matrix.append(nums)
+
+    result = 0
+    x_max = len(matrix)
+    for i in xrange(x_max):
+        y_max = len(matrix[i])
+        for j in xrange(len(matrix[i])):
+            result = max(cal_sub_max_product(matrix, i, j, x_max, y_max), result)
+
+    return result
+
+def run():
+    p11()
+
+if __name__ == "__main__":
+    run()
