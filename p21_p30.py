5358a313c6b442d5b9d316a6011b885e80bd453f
lycheng <lycheng997@gmail.com>
2013-10-03 15:22:58 +0800

    p23

--- p21_p30.py
+++ p21_p30.py
@@ -65,8 +65,39 @@ def p22():
     return score
 
 
+@timeit(times=1)
+def p23():
+
+    def get_factors(n):
+        rv = set(reduce(list.__add__,
+                            ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+        rv.remove(n)
+        return rv
+
+    upper_limit = 28123
+    abundant_nums = set()
+    except_nums = set()
+
+    for i in range(2, upper_limit + 1):
+        sum_of_factors = sum(get_factors(i))
+        if sum_of_factors > i:
+            abundant_nums.add(i)
+
+    for i in abundant_nums:
+        for j in abundant_nums:
+            except_nums.add(i + j)
+
+    result = 0
+    for i in range(upper_limit + 1):
+        if i in except_nums:
+            continue
+        result = result + i
+
+    return result
+
+
 def run():
-    p22()
+    p23()
 
 
 if __name__ == "__main__":
