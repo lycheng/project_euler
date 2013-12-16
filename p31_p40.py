33b8bfd0a58a86f6f99567b338f31ac0accb68a3
lycheng <lycheng997@gmail.com>
2013-12-16 23:20:20 +0800

    finished p32

--- p31_p40.py
+++ p31_p40.py
@@ -4,8 +4,8 @@
 __author__ = "lycheng"
 __email__ = "lycheng997@gmail.com"
 
-
 from utils import timeit
+
 @timeit(times=1)
 def p31():
     target = 200
@@ -20,8 +20,32 @@ def p31():
     return ways[-1]
 
 
+@timeit(times=1)
+def p32():
+    def is_pandigital(digits):
+        if len(digits) != 9:
+            return False
+        for i in range(1, 10):
+            if str(i) not in digits:
+                return False
+        return True
+
+    rv = set()
+    for i in range(2, 99):
+        for j in range(i + 1, 10000):
+            digits = "%d%d%d" % (i, j, i * j)
+            if len(digits) > 9:
+                continue
+
+            if not is_pandigital(digits):
+                continue
+            rv.add(i * j)
+
+    return sum(rv)
+
+
 def run():
-    p31()
+    p32()
 
 
 if __name__ == "__main__":
